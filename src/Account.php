6654275f966cb42dfc4c35281ce8075f59b4e43c
Jan Jablunka <jablunka@gmail.com>
2024-08-13 17:30:20 +0200

    incremental

--- src/Account.php
+++ src/Account.php
@@ -2,98 +2,114 @@
 
 namespace Pankki;
 
-class Account
+use Katu\Tools\Options\OptionCollection;
+use Katu\Tools\Rest\RestResponse;
+use Katu\Tools\Rest\RestResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Account implements RestResponseInterface
 {
-  protected $accountNumber;
-  protected $bankCode;
-  protected $countryCode;
+	protected $accountNumber;
+	protected $bankCode;
+	protected $countryCode;
 
-  public function __construct(AccountNumber $accountNumber, BankCode $bankCode, ?CountryCode $countryCode = null)
-  {
-    $this->setAccountNumber($accountNumber);
-    $this->setBankCode($bankCode);
-    $this->setCountryCode($countryCode);
-  }
+	public function __construct(AccountNumber $accountNumber, BankCode $bankCode, ?CountryCode $countryCode = null)
+	{
+		$this->setAccountNumber($accountNumber);
+		$this->setBankCode($bankCode);
+		$this->setCountryCode($countryCode);
+	}
 
-  public function __toString(): string
-  {
-    return $this->getFormatted();
-  }
+	public function __toString(): string
+	{
+		return $this->getFormatted();
+	}
 
-  public static function createFromString(string $string)
-  {
-    list($accountNumberString, $bankCodeString) = explode("/", $string);
+	public static function createFromString(string $string)
+	{
+		list($accountNumberString, $bankCodeString) = explode("/", $string);
 
-    return new Account(
-      AccountNumber::createFromString($accountNumberString),
-      new BankCode($bankCodeString),
-    );
-  }
+		return new Account(
+			AccountNumber::createFromString($accountNumberString),
+			new BankCode($bankCodeString),
+		);
+	}
 
-  public function setAccountNumber(AccountNumber $accountNumber): Account
-  {
-    $this->accountNumber = $accountNumber;
+	public function setAccountNumber(AccountNumber $accountNumber): Account
+	{
+		$this->accountNumber = $accountNumber;
 
-    return $this;
-  }
+		return $this;
+	}
 
-  public function getAccountNumber(): AccountNumber
-  {
-    return $this->accountNumber;
-  }
+	public function getAccountNumber(): AccountNumber
+	{
+		return $this->accountNumber;
+	}
 
-  public function setBankCode(BankCode $bankCode): Account
-  {
-    $this->bankCode = $bankCode;
+	public function setBankCode(BankCode $bankCode): Account
+	{
+		$this->bankCode = $bankCode;
 
-    return $this;
-  }
+		return $this;
+	}
 
-  public function getBankCode(): BankCode
-  {
-    return $this->bankCode;
-  }
+	public function getBankCode(): BankCode
+	{
+		return $this->bankCode;
+	}
 
-  public static function getDefaultCountryCode(): CountryCode
-  {
-    return new CountryCode("CZ");
-  }
+	public static function getDefaultCountryCode(): CountryCode
+	{
+		return new CountryCode("CZ");
+	}
 
-  public function setCountryCode(?CountryCode $countryCode): Account
-  {
-    $this->countryCode = $countryCode;
+	public function setCountryCode(?CountryCode $countryCode): Account
+	{
+		$this->countryCode = $countryCode;
 
-    return $this;
-  }
+		return $this;
+	}
 
-  public function getCountryCode(): ?CountryCode
-  {
-    return $this->countryCode;
-  }
+	public function getCountryCode(): ?CountryCode
+	{
+		return $this->countryCode;
+	}
 
-  public function getResolvedCountryCode(): ?CountryCode
-  {
-    return $this->getCountryCode() ?: $this->getDefaultCountryCode();
-  }
+	public function getResolvedCountryCode(): ?CountryCode
+	{
+		return $this->getCountryCode() ?: $this->getDefaultCountryCode();
+	}
 
-  public function getFormatted(): string
-  {
-    return implode("/", array_filter([
-      $this->getAccountNumber()->getFormatted(),
-      $this->getBankCode()->getFormatted(),
-    ]));
-  }
+	public function getFormatted(): string
+	{
+		return implode("/", array_filter([
+			$this->getAccountNumber()->getFormatted(),
+			$this->getBankCode()->getFormatted(),
+		]));
+	}
 
-  public function getIBAN(): IBAN
-  {
-    $iban = new \PHP_IBAN\IBAN(implode([
-      $this->getResolvedCountryCode(),
-      "00",
-      $this->getBankCode()->getStandardized(),
-      $this->getAccountNumber()->getStandardized(),
-    ]));
-    $iban->setChecksum();
+	public function getIBAN(): IBAN
+	{
+		$iban = new \PHP_IBAN\IBAN(implode([
+			$this->getResolvedCountryCode(),
+			"00",
+			$this->getBankCode()->getStandardized(),
+			$this->getAccountNumber()->getStandardized(),
+		]));
+		$iban->setChecksum();
 
-    return new IBAN($iban->iban);
-  }
+		return new IBAN($iban->iban);
+	}
+
+	public function getRestResponse(?ServerRequestInterface $request = null, ?OptionCollection $options = null): RestResponse
+	{
+		return new RestResponse([
+			"iban" => $this->getIBAN()->getIBAN(),
+			"accountNumber" => $this->getAccountNumber()->getNumber(),
+			"bankCode" => $this->getBankCode()->getCode(),
+			"countryCode" => $this->getCountryCode()->getCode(),
+			"formatted" => $this->getFormatted(),
+		]);
+	}
 }
--- src/IBAN.php
+++ src/IBAN.php
@@ -27,4 +27,33 @@ class IBAN
 	{
 		return $this->iban;
 	}
+
+	public function getPHPIBAN(): \PHP_IBAN\IBAN
+	{
+		return new \PHP_IBAN\IBAN($this->getIBAN());
+	}
+
+	public function getAccountNumber(): AccountNumber
+	{
+		return new AccountNumber($this->getPHPIBAN()->Account());
+	}
+
+	public function getBankCode(): BankCode
+	{
+		return new BankCode($this->getPHPIBAN()->Bank());
+	}
+
+	public function getCountryCode(): CountryCode
+	{
+		return new CountryCode($this->getPHPIBAN()->Country());
+	}
+
+	public function getAccount(): Account
+	{
+		return new Account(
+			$this->getAccountNumber(),
+			$this->getBankCode(),
+			$this->getCountryCode(),
+		);
+	}
 }
